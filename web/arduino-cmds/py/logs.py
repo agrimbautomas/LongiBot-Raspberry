b0a65cd478daa73c9d7dda19a707a05b8f063043
Tomas Agrimbau <agrimbautomas@gmail.com>
2020-05-16 14:31:11 -0300

    Added log files

---
+++ web/arduino-cmds/py/logs.py
@@ -0,0 +1,16 @@
+import logging
+import datetime
+
+# logging  
+LOG = "../../iflan.log"                                                     
+logging.basicConfig(filename=LOG, filemode="w", level=logging.DEBUG)  
+
+# console handler  
+console = logging.StreamHandler()  
+console.setLevel(logging.ERROR)  
+logging.getLogger("").addHandler(console)
+
+def iflan_log(message):
+	now = datetime.datetime.now()
+	parsed_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
+	logging.debug(parsed_datetime + ": " + message)
--- web/arduino-cmds/py/start_serial.py
+++ web/arduino-cmds/py/start_serial.py
@@ -1,9 +1,10 @@
 import serial
 from config import ACM_PORT 
+from logs import iflan_log
 
 ser=serial.Serial(ACM_PORT,9600)  #change ACM number as found from ls /dev/tty/ACM*
 ser.baudrate=9600
 
 while True:
 	read_ser=ser.readline()
-	print(read_ser)
+	iflan_log(read_ser.decode("utf-8"))
