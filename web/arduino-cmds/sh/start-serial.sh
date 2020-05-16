01f87c73bf2ba0c46515db59366b354a9c766abb
Tomas Agrimbau <tomas@amalgama.co>
2020-05-16 13:22:26 -0300

    Move views to views dir

---
+++ web/arduino-cmds/sh/start-serial.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python3 /home/pi/longibot-raspberry/web/arduino-cmds/py/start-serial.py
+
+
--- web/js/joystick.js
+++ web/js/joystick.js
@@ -1,10 +1,10 @@
 
 $(document).ready(function () {
-    setupButton('#joystick-stop-button', 'arduino-cmds/php/stop.php');
-    setupButton('#joystick-button2', 'arduino-cmds/php/move_forward.php');
-    setupButton('#joystick-button4', 'arduino-cmds/php/move_left.php');
-    setupButton('#joystick-button5', 'arduino-cmds/php/move_right.php');
-    setupButton('#joystick-button7', 'arduino-cmds/php/move_backward.php');
+    setupButton('#joystick-stop-button', '../arduino-cmds/php/stop.php');
+    setupButton('#joystick-button2', '../arduino-cmds/php/move_forward.php');
+    setupButton('#joystick-button4', '../arduino-cmds/php/move_left.php');
+    setupButton('#joystick-button5', '../arduino-cmds/php/move_right.php');
+    setupButton('#joystick-button7', '../arduino-cmds/php/move_backward.php');
 });
 
 function setupButton(selector, file) {
