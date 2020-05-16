28ecab51f1d71937e0778d489d7da1c54e0153e1
Tomas Agrimbau <agrimbautomas@gmail.com>
2020-05-16 13:36:59 -0300

    Minify serial method

--- web/arduino-cmds/py/start_serial.py
+++ web/arduino-cmds/py/start_serial.py
@@ -1,13 +1,8 @@
 import serial
-import RPi.GPIO as GPIO
-import time
 from config import ACM_PORT 
 
 ser=serial.Serial(ACM_PORT,9600)  #change ACM number as found from ls /dev/tty/ACM*
 ser.baudrate=9600
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
 
 while True:
 	read_ser=ser.readline()
