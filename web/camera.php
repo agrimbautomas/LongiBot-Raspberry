2edb996fc91c69ad98ffeba633d298072552060f
Tomas Agrimbau <tomas@amalgama.co>
2020-05-24 18:05:45 -0300

    Add buttons for camera

--- web/camera.php
+++ web/camera.php
@@ -3,4 +3,8 @@ require 'config.php';
 ?>
 <div class="camera-container containers-frame-screen hidden-frame" id="camera-container">
     <iframe src="http://<?php echo RASPBERRY_IP; ?>/camera/web/"></iframe>
+    <div class="camera-servo-buttons">
+        <i class="fas fa-arrow-up" id="camera-servo-move-up"></i>
+        <i class="fas fa-arrow-down" id="camera-servo-move-down"></i>
+    </div>
 </div>
