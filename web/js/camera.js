c6695ec151c8ee57f1870069714bf8f07077ed7d
Tomas Agrimbau <tomas@amalgama.co>
2020-05-24 20:06:45 -0300

    Refactor script

--- web/js/camera.js
+++ web/js/camera.js
@@ -9,33 +9,15 @@ $(document).ready(function () {
 });
 
 function setupCameraButtons() {
-    $upCameraButton.click(function () {
-        modeCameraUp();
-    });
-    $downCameraButton.click(function () {
-        modeCameraDown();
-    });
+    $upCameraButton.click(modeCameraUp);
+    $downCameraButton.click(modeCameraDown);
 }
 
 
 function modeCameraUp() {
-    $.get({
-        url: "arduino-cmds/php/move_camera_up.php",
-        success: function (data) {
-            let result = $.parseJSON(data);
-            console.log("data", data);
-            console.log("result", result);
-        }
-    });
+    $.get({url: "arduino-cmds/php/move_camera_up.php"});
 }
 
 function modeCameraDown() {
-    $.get({
-        url: "arduino-cmds/php/move_camera_down.php",
-        success: function (data) {
-            let result = $.parseJSON(data);
-            console.log("data", data);
-            console.log("result", result);
-        }
-    });
+    $.get({url: "arduino-cmds/php/move_camera_down.php"});
 }
