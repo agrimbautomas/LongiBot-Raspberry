1f2de0abd106bf1d59dc4791fb016d9717979ee4
Tomas Agrimbau <agrimbautomas@gmail.com>
2020-05-24 12:21:50 -0300

    Add permissons for automatic serial start

--- web/arduino-cmds/sh/start_serial.sh
+++ web/arduino-cmds/sh/start_serial.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-nohup python3 /home/pi/longibot-raspberry/web/arduino-cmds/py/start_serial.py >> ../../logs/serial.log &
-
-
+nohup python3 /home/pi/longibot-raspberry/web/arduino-cmds/py/start_serial.py >> /home/pi/longibot-raspberry/web/logs/serial.log &
