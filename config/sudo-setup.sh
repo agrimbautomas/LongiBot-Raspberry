5a9cfd274408cd6413e15d07435dbf6db0720edb
Tomas Agrimbau <agrimbautomas@gmail.com>
2020-06-13 20:53:38 -0300

    Add nginx to config

--- config/sudo-setup.sh
+++ config/sudo-setup.sh
@@ -1,18 +1,15 @@
-# Install vim
-sudo apt install vim
-
 # Add vim styles
 sudo cp /home/pi/longibot-raspberry/config/.vimrc /root/
 
-# Install apache2
+# Install apache2 and vim
 sudo apt update
-sudo apt install apache2 -y
+sudo apt install nginx vim -y
 
-# Install php
-sudo apt install php libapache2-mod-php -y
+# Start Nginx
+sudo /etc/init.d/nginx start
 
 # Add apache confi to run from pi dir
-sudo cat /home/pi/longibot-raspberry/config/apache-custom.txt >> /etc/apache2/apache2.conf
+#sudo cat /home/pi/longibot-raspberry/config/apache-custom.txt >> /etc/apache2/apache2.conf
 
 # Add apache2 user to group to dialout group
 sudo usermod -a -G dialout www-data
