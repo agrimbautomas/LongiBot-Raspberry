36914557a0e04c938eb54355cc2a6ece470c9af8
Tomas Agrimbau <tomas@amalgama.co>
2020-06-15 01:06:22 -0300

    Add js method to display on screen

--- web/camera.php
+++ web/camera.php
@@ -3,12 +3,24 @@ require 'config.php';
 ?>
 <div class="camera-container containers-frame-screen hidden-frame" id="camera-container">
     <!-- HTML -->
-    <video id='raspi-cam'  class="video-js vjs-default-skin" width="400" height="300" controls>
+    <video id='raspi-cam' class="video-js vjs-default-skin" width="400" height="300" controls>
         <source type="application/x-mpegURL" src="http://<?php echo RASPBERRY_IP; ?>/hls/index.m3u8">
     </video>
 
     <div class="camera-servo-buttons">
         <i class="fas fa-arrow-up" id="camera-servo-move-up"></i>
         <i class="fas fa-arrow-down" id="camera-servo-move-down"></i>
+    </div>
+
+    <div class="camera-data-panel">
+        <div>
+            <i class="fas fa-tint"></i>
+            <span id="humidity-data"></span>
+        </div>
+        <div>
+            <i class="fas fa-thermometer-half"></i>
+            <span id="temperature-data"></span>
+        </div>
+
     </div>
 </div>
--- web/js/camera.js
+++ web/js/camera.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
 
     setupCameraPlayer();
     setupCameraButtons();
+    updateTempHum();
 });
 
 function setupCameraPlayer() {
@@ -26,4 +27,33 @@ function modeCameraUp() {
 
 function modeCameraDown() {
     $.get({url: "arduino-cmds/php/move_camera_down.php"});
+}
+
+
+function updateTempHum() {
+    getTempHumFromFile();
+    setInterval(function () {
+        getTempHumFromFile();
+    }, 60000);
+}
+
+function getTempHumFromFile() {
+    $.get({
+        url: "logs/temp-hum.json",
+        success: function (data) {
+            // Parse split from slash
+            let tmpHumArr = data['temp/hum'].split("/");
+            let hum = tmpHumArr[0];
+            let temp = tmpHumArr[1];
+            displayTempHum(hum, temp);
+        }
+    });
+}
+
+function displayTempHum(hum, temp) {
+    hum = parseFloat(hum).toFixed(1);
+    temp = parseFloat(temp).toFixed(1);
+
+    $('#humidity-data').html(hum + "%");
+    $('#temperature-data').html(temp + "°");
 }
