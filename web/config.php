1ba474249e1470902dbde058e200e0856d8dc1fe
Tomas Agrimbau <tomas@amalgama.co>
2020-08-29 18:37:06 -0300

    Add if function exits

--- web/config.php
+++ web/config.php
@@ -1,11 +1,13 @@
 <?php
 
-define('RASPBERRY_IP', $_SERVER['SERVER_ADDR'] === "::1" ? '192.168.0.13' : $_SERVER['SERVER_ADDR'] );
+define('RASPBERRY_IP', $_SERVER['SERVER_ADDR'] === "::1" ? '192.168.0.3' : $_SERVER['SERVER_ADDR']);
 define('HOST', get_current_user() != 'pi' ? 'ssh pi@' . RASPBERRY_IP : "");
 define('ARDUINO_SH__PATH', '/home/pi/longibot-raspberry/web/arduino-cmds/sh/');
 
-function runArduinoShCmd($fileName){
-	$filePath = ARDUINO_SH__PATH . $fileName;
-	$cmd = HOST . " bash $filePath";
-	return exec($cmd);
+if ( !function_exists('runArduinoShCmd') ){
+	function runArduinoShCmd( $fileName ){
+		$filePath = ARDUINO_SH__PATH . $fileName;
+		$cmd = HOST . " bash $filePath";
+		return exec($cmd);
+	}
 }
